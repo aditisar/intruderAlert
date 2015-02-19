a6cdeec4ed26d5260301f5aebf893bf792b0159a
Aditi Sarkar <aditi0821@gmail.com>
2015-02-19 06:33:57 -0500

    done

--- public/sketch.js
+++ public/sketch.js
@@ -1,27 +1,20 @@
-var text; // variable for the text div you'll create
 var oldIntruderCount = 0;
-var a = 30;
-var b = 30;
+
 function setup() {
   // make a new div and position it at 10, 10:
-  createCanvas($(window).height(), $(window).width());
-
-  text = createDiv("Sensor reading:");
   // make a HTTP call to the server for the data:
   var sensorReading = loadStrings("/data", showData);
 }
  
 function showData(result) {
    // when the server returns, show the result in the div:
-   text.html("Sensor reading: " + result);
-   text.position(10, 10);
+   $("#count").html(result + " intruders");
    // make another HTTP call:
-   intruderCount = loadStrings("/count", showData);
    if (result>oldIntruderCount){
-      ellipse(a,b,30,30);
-      a+=40;
-      b+=40;
+      $("#stage").append("<div class='traveler'><div class='bouncer'><!-- --></div></div>"); 
+      $(".traveler").last().css( "-webkit-animation-duration", "3s" );
       oldIntruderCount = result;
    }
-}
+   intruderCount = loadStrings("/count", showData);
 
+}
